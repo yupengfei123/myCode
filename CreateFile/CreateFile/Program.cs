f5357bdd2178292a7e068e880794535717016d01
yupengfei123 <13813297238@163.com>
2017-02-14 12:42:09 +0800

    Add files via upload

---
+++ CreateFile/CreateFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\Users\\Administrator\\Desktop\\项目\\Shift.txt",FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            Random ranMileage = new Random();
+            int shift;
+            for(int n=0;n<10000;n++)
+            {
+                shift=ranMileage.Next(1,20);
+                sw.WriteLine(shift.ToString());
+            }
+            sw.Close();
+            fs.Close();
+        }
+    }
+}
